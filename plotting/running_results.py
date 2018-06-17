5545b09fe7b4f249fe4c97c7054293f040bb1cb6
moshe9362 <moshe9362@gmail.com>
2018-06-17 15:10:23 +0300

    Create objects to easy plotting of the data

---
+++ plotting/running_results.py
@@ -0,0 +1,24 @@
+import json
+
+
+class RunningResults:
+
+    def __init__(self, json_to_open="output_template.json"):
+        with open(json_to_open) as j:
+            self.data = json.load(j)
+            self.name = self.data["name"]
+            self.stamps = []
+            for stamp_arr in self.data["stamps"]:
+                self.stamps.append(Stamp(stamp_arr["time_stamp"], stamp_arr["delay"]))
+
+
+class Stamp:
+    def __init__(self, time_stamp, delays):
+        self.time_stamp = time_stamp
+        self.delays = delays
+
+
+if __name__ == '__main__':
+    res = RunningResults()
+
+
